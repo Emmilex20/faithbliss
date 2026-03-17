330912fceb6ef1fcaa894614519f78932ffaf075
Agina Emmanuel <aginaemmanuel6@gmail.com>
2026-03-17 01:11:53 +0100

    add www to cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -31,7 +31,7 @@ import { initializeSocketIO } from './socket/socket';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', 'https://faithblissafrica.com', process.env.CLIENT_URL].filter(
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174', 'https://faithblissafrica.com', 'https://www.faithblissafrica.com', process.env.CLIENT_URL].filter(
   (origin): origin is string => Boolean(origin),
 );
 
